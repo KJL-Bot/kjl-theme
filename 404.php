1bc784a74df0a5075988a879aa2ee38b17d9414f
Gabriel Serwas <post@gabrielserwas.com>
2023-02-10 09:13:26 +0100

    Add simple content for 404 page.

--- 404.php
+++ 404.php
@@ -13,20 +13,20 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
-			<header class="page-header">
+			<!-- <header class="page-header">
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'kjl-theme' ); ?></h1>
-			</header><!-- .page-header -->
+			</header>.page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'kjl-theme' ); ?></p>
+				<p><?php esc_html_e( 'Tut uns leid, aber diese Seite bzw. dieses Buch gibt\'s nicht.', 'kjl-theme' ); ?></p>
 
 					<?php
-					get_search_form();
+					// get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
+					// the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
-					<div class="widget widget_categories">
+					<!-- <div class="widget widget_categories">
 						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'kjl-theme' ); ?></h2>
 						<ul>
 							<?php
@@ -41,14 +41,15 @@ get_header();
 							);
 							?>
 						</ul>
-					</div><!-- .widget -->
+					</div> -->
+					<!-- .widget -->
 
 					<?php
 					/* translators: %1$s: smiley */
-					$kjl_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'kjl-theme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$kjl_theme_archive_content" );
+					// $kjl_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'kjl-theme' ), convert_smilies( ':)' ) ) . '</p>';
+					// the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$kjl_theme_archive_content" );
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
+					// the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 			</div><!-- .page-content -->
