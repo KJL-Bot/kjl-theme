cb8477fb0e8c872e063bf4fe7399b559da105b0c
Gabriel Serwas <post@gabrielserwas.com>
2023-02-10 09:13:49 +0100

    Add header image for 404 page.

--- header.php
+++ header.php
@@ -15,6 +15,14 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="KJL-Bot-Team">
+	<meta property="og:description" content="<?php bloginfo('description'); ?>">
+	<!-- Twitter open-graph tags -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:image:alt" content="KJL-Bot">
+	<meta name="twitter:title" content="<?php bloginfo( 'name' ); ?>">
+	<meta name="twitter:description" content="<?php bloginfo('description'); ?>">
+	<meta name="twitter:image" content="https://kjl-bot.de/wp-content/uploads/2023/02/KJL-Bot-Page.png">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
@@ -40,6 +48,11 @@
 			?>
 		</nav><!-- #site-navigation -->
 		<div class="site-branding" style="//background-image: url('<?php header_image(); ?>')">
+		<?php if(!is_404()) { ?>
 			<img alt="" src="<?php header_image(); ?>" width="auto<?php //echo absint( get_custom_header()->width ); ?>" height="auto<?php //echo absint( get_custom_header()->height ); ?>">
+		<?php } ?>
+		<?php if(is_404()) { ?>
+			<img alt="" src="<?php echo get_template_directory_uri() . '/images/leeres_regal.jpg'; ?>" width="auto" height="auto" style="width: 100%;">
+		<?php } ?>
 		</div><!-- .site-branding -->
 	</header><!-- #masthead -->
