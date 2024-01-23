13c551f2acdce1f09dff5dab79855b3a06193855
Gabriel Serwas <post@gabrielserwas.com>
2024-01-23 21:19:25 +0100

    Insert shortcode for kjl-bot-filter

--- search.php
+++ search.php
@@ -13,7 +13,7 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
-
+			<?= do_shortcode('[kjl-bot-filter]'); ?>
 			<header class="page-header">
 				<h1 class="page-title">
 					<?php
